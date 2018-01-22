4cbf286451b393e2412842c79848fa73e8f93498
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-22 22:32:31 +0000

    Fix plugin initialize

--- lib/spina/projects/engine.rb
+++ lib/spina/projects/engine.rb
@@ -1,9 +1,9 @@
 module Spina
   module Projects
     class Engine < ::Rails::Engine
-      initializer 'spina.plugin.register.spina_projects', before: :load_config_initializers do
+      config.before_initialize do
         ::Spina::Plugin.register do |plugin|
-          plugin.name = 'Projects'
+          plugin.name = 'projects'
           plugin.namespace = 'projects'
         end
       end
