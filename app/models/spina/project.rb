347509cbb2b3f751f24dd31913ce69cefe21a3f2
Dan Mitchell <dan@danmitchell.co.uk>
2019-07-17 12:51:50 +0100

    Remove requirement for lat/long

--- app/models/spina/project.rb
+++ app/models/spina/project.rb
@@ -8,7 +8,7 @@ module Spina
     belongs_to :image_collection, optional: true, class_name: 'Spina::ImageCollection'
     belongs_to :image, class_name: 'Spina::Image'
 
-    validates :title, :description, :lat, :long, :image, presence: true
+    validates :title, :description, :image, presence: true
     validates :slug, uniqueness: true
     accepts_nested_attributes_for :image_collection
 
