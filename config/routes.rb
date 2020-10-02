63f752034d88954beb32e424a4c597a053d6cc4c
markmead <itsmarkmead@gmail.com>
2020-10-02 09:07:07 +0100

    remove custom JS for project sorting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Spina::Engine.routes.draw do
+  resources :projects
+
   namespace :admin do
     resources :projects do
-      put :sort, on: :collection
+      post :sort, on: :collection
     end
   end
 end
