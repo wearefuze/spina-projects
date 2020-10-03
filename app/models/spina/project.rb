0b042176c83292718ca9abaa93610e3bbd8e70d2
Mark Mead <50486078+markmead@users.noreply.github.com>
2020-10-03 11:57:24 +0100

    Update gems & clean up (#96)
    * run bundle update
    
    * fix friendlyid & clean up

--- app/models/spina/project.rb
+++ app/models/spina/project.rb
@@ -1,33 +1,31 @@
 module Spina
   class Project < ActiveRecord::Base
-    include FriendlyId
-
-    friendly_id :title, use: :slugged
+    extend FriendlyId
+    friendly_id :title, use: [:slugged, :finders]
 
     belongs_to :project_category, optional: true
-    belongs_to :image_collection, optional: true, class_name: 'Spina::ImageCollection'
-    belongs_to :image, class_name: 'Spina::Image'
+    belongs_to :image_collection, optional: true, class_name: "Spina::ImageCollection"
+    belongs_to :image, class_name: "Spina::Image"
+
+    accepts_nested_attributes_for :image_collection
 
     validates :title, :description, :image, :publish_date, presence: true
     validates :slug, uniqueness: true
-    accepts_nested_attributes_for :image_collection
 
-    scope :live, -> { where('publish_date <= ? AND draft = ?', Date.today, 0) }
-    scope :newest_first, -> { order('publish_date DESC') }
+    scope :by_newest, -> { order(publish_date: :DESC) }
+    scope :by_position, -> { order(position: :ASC) }
+    scope :is_live, -> { where("publish_date <= ? AND draft = ?", Date.today, 0) }
 
     def live?
-      true if self.publish_date <= Date.today && draft == 0
+      self.publish_date <= Date.today && draft.zero?
     end
 
     def scheduled?
-      true if self.publish_date >= Date.today && draft == 0
+      self.publish_date >= Date.today && draft.zero?
     end
 
     def draft?
       draft == 1
     end
-
-    scope :by_position, -> { order('position ASC') }
-
   end
 end
